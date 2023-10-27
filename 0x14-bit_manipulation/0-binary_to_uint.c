2b3ebfceaa1ab1b22b5acd586294d29367108c46
Patience Angbas <patienceangbas@gmail.com>
2023-10-26 18:09:14 -0700

    C - Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int a;
-unsigned int num;
-num = 0;
-if (!b)
-return (0);
-for (a = 0; b[a] != '\0'; a++)
+unsigned int len;
+size_t i = 0;
+size_t j = 0;
+size_t sum = 0;
+size_t pow = 1;
+int base = 2;
+if (b == NULL)
+	return (0);
+for (len = 0; b[len] != '\0'; len++)
+	;
+if (len == 1 && (b[0] == '0' || b[0] == '1'))
+	return (b[0] - 48);
+for (i = 0; b[i] != '\0'; i++)
 {
-if (b[a] != '0' && b[a] != '1')
-return (0);
+if (b[i] != '0' && b[i] != '1')
+	return (0);
 }
-for (a = 0; b[a] != '\0'; a++)
-{
-num <<= 1;
-if (b[a] == '1')
-num += 1;
 }
-return (num);
-}
+
